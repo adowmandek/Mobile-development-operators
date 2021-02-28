f6e7889e4c171c6d5c6fed37f47582b44e6f50ef
adowmandek <adowmandek@gmail.com>
2021-02-28 18:12:20 +0300

    first commit

---
+++ src/Rukz.kt
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+fun main() {
+   println("Hello mandek")
+    division()
+    add()
+    println(true)
+}
+
+fun mandek() {
+    println("Hello mandek")
+}
+fun division() {
+    var num1=21
+    var num2=20
+    var sum=num2/num2
+    println(sum)
+}
+fun add() {
+    var num1=2
+    var num2=4
+    var num3=6
+    var num4=10
+    var sum=num1+num2+num3+num3
+    println(sum)
+}
+fun me() {
+    var me="mandek is happy"
+    println(true)
+}
